d6dc44ea609189be4df77efafd67a23162b503e7
silabs-MarkOW <41750418+silabs-MarkOW@users.noreply.github.com>
2024-04-03 18:11:46 -0500

    close

---
+++ protected_apploader_ota/inc/external_signals.h
@@ -0,0 +1,12 @@
+/*
+ * Define labels for bits in external_signals bitmap
+ */
+
+#ifndef EXTERNAL_SIGNALS_H_
+#define EXTERNAL_SIGNALS_H_
+
+#define BUTTON_PRESS     (1u)
+#define BUTTON_RELEASE   (2u)
+#define TIMEOUT_TO_CLOSE (4u)
+
+#endif /* EXTERNAL_SIGNALS_H_ */

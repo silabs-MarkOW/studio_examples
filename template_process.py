af023d91285294cd4c6f2e954ed34f10c70b99db
silabs-MarkOW <41750418+silabs-MarkOW@users.noreply.github.com>
2024-05-07 20:09:53 -0500

    clean up readme to template.xml

--- template_process.py
+++ template_process.py
@@ -2,6 +2,24 @@ import os
 from lxml import etree
 from collections import OrderedDict
 
+def get_readme(db,slcp) :
+    if dict != type(db) :
+        raise RuntimeError
+    l = db.get('readme')
+    if None == l :
+        return None
+    if not list == type(l) :
+        raise RuntimeError
+    if 1 != len(l) :
+        print('Haven\'t implemented handling is more than 1 readme ... use first')
+    d = l[0]
+    if dict != type(d) :
+        raise RuntimeError
+    p =  d.get('path')
+    if None == p :
+        return p
+    path = os.path.dirname(slcp)
+    return path + '/' + p
 class TemplateXML :
     def __init__(self, path) :
         print('TemplateXML:loading %s'%(path))
@@ -37,6 +55,7 @@ class TemplateXML :
         orderedDict = OrderedDict([("name",app['project_name']),("label",app['label']),("description",app['description'])])
         descriptors = etree.SubElement(self.doc.getroot(), 'descriptors', orderedDict)
         got_quality = False
+        got_readme = False
         for p in descriptor.iter('properties') :
             key = p.attrib.get('key')
             value = p.attrib.get('value')
@@ -50,11 +69,19 @@ class TemplateXML :
             if 'quality' == key :
                 value = app['quality'].upper()
                 got_quality = True
+            if 'readmeFiles' == key :
+                value = get_readme(app,slcp)
+                got_readme = True
             properties = etree.SubElement(descriptors, 'properties',
                                           key=key, value=value)
         if not got_quality :
             properties = etree.SubElement(descriptors, 'properties',
                                           key='quality', value=app['quality'].upper())
+        if not got_readme :
+            readme = get_readme(app,slcp)
+            if None != readme :
+                properties = etree.SubElement(descriptors, 'properties',
+                                              key='readmeFiles', value=readme)
     def write(self,path) :
         self.doc.write(path, pretty_print=True, xml_declaration=True)#,encoding='utf-8')
         return
